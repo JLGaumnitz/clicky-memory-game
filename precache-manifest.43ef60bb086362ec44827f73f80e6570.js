a646ad02620f0ef86d375fbf28269c375859288f
JLG <mjgaumnitz@kc.rr.com>
2020-01-29 23:14:47 -0600

    Updates

---
+++ precache-manifest.43ef60bb086362ec44827f73f80e6570.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99811c1ee5400a28bbad7ed9f04b02a6",
+    "url": "/clicky-memory-game/index.html"
+  },
+  {
+    "revision": "cb70e9570bf2a06c1c3c",
+    "url": "/clicky-memory-game/static/css/main.77e2a5c7.chunk.css"
+  },
+  {
+    "revision": "f1f2fbe379beff050dfd",
+    "url": "/clicky-memory-game/static/js/2.dbaea621.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-memory-game/static/js/2.dbaea621.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cb70e9570bf2a06c1c3c",
+    "url": "/clicky-memory-game/static/js/main.80537ed7.chunk.js"
+  },
+  {
+    "revision": "1626cea91ac85ff706ba",
+    "url": "/clicky-memory-game/static/js/runtime-main.00513e4d.js"
+  },
+  {
+    "revision": "9f2f0df522b6b4bcf707a5b4c69cac28",
+    "url": "/clicky-memory-game/static/media/Adventures_of_Pippi_Longstocking.9f2f0df5.jpg"
+  },
+  {
+    "revision": "f9e42b0025e6b7047e2f14c6d55f3c52",
+    "url": "/clicky-memory-game/static/media/Black_Beauty.f9e42b00.jpg"
+  },
+  {
+    "revision": "507d8fc418ece0fb6568eb815c0beba5",
+    "url": "/clicky-memory-game/static/media/Boxcar_Children.507d8fc4.jpg"
+  },
+  {
+    "revision": "b344fe59355e7b97f56460f9524069dc",
+    "url": "/clicky-memory-game/static/media/Charlie_and_the_Chocolate_Factory.b344fe59.jpg"
+  },
+  {
+    "revision": "6c977301dc616388f0238eaba2725951",
+    "url": "/clicky-memory-game/static/media/Charlottes_Web.6c977301.jpg"
+  },
+  {
+    "revision": "cb8e66bf6785968f3f07d715e1b53c83",
+    "url": "/clicky-memory-game/static/media/Heidi.cb8e66bf.jpg"
+  },
+  {
+    "revision": "d952d1afee7f85370039b7681fa58624",
+    "url": "/clicky-memory-game/static/media/Little_Women.d952d1af.jpg"
+  },
+  {
+    "revision": "db8ced05b0716a9ba221b1ef5591d302",
+    "url": "/clicky-memory-game/static/media/The_Hobbit.db8ced05.jpg"
+  },
+  {
+    "revision": "83b80cbae07ea9bd075e546ca1705d38",
+    "url": "/clicky-memory-game/static/media/The_Lion_the_Witch_and_the_Wardrobe.83b80cba.jpg"
+  },
+  {
+    "revision": "db24fdf32811ec7d13e0ef39d36beb1c",
+    "url": "/clicky-memory-game/static/media/The_Wizard_of_Oz.db24fdf3.jpg"
+  },
+  {
+    "revision": "0266ad477c23470836d439dc6d1f21a1",
+    "url": "/clicky-memory-game/static/media/Wind_in_the_Willows.0266ad47.jpg"
+  },
+  {
+    "revision": "ef33f47c75e97d2c7404e1a061a7c636",
+    "url": "/clicky-memory-game/static/media/Winnie_the_Pooh.ef33f47c.jpg"
+  },
+  {
+    "revision": "e5e7c0a3bb8fb939be73b63ae416e61b",
+    "url": "/clicky-memory-game/static/media/bookswallpaper.e5e7c0a3.jpg"
+  }
+]);
